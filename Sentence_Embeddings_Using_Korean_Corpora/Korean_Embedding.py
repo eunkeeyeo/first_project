9a5162bb1794f9602011dbbb7170e695bb6b8767
eunkeeyeo <eunkeeyeo@gmail.com>
2022-01-14 13:44:24 +0900

    remove files

--- Sentence_Embeddings_Using_Korean_Corpora/Korean_Embedding.py
+++
@@ -1,6 +0,0 @@
-import tensorflow
-tf.compat.v1.disable_v2_behavior()
-
-message = tf.constant('hello tensorflow')
-session = tf.compat.v1.Session()
-print(session.run(message))
--- Sentence_Embeddings_Using_Korean_Corpora/korean.py
+++
@@ -1,4 +0,0 @@
-from konlpy.tag import Mecab
-
-tokenizer = Mecab()
-tokenizer.morphs("아버지가방에들어가신다")
